602aae36a0cd9833be7eacbb3a66df7460e29b88
Anuj Doshi <anuj.doshi.sa@gmail.com>
2020-01-27 16:09:17 +0530

    Data persists use of user defaults.

--- Todo/TableViewController.swift
+++ Todo/TableViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class TableViewController: UITableViewController {
     var itemArray = ["Buy Apple","Buy Milk"]
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]
+        {
+                itemArray = items
+        }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -33,5 +37,22 @@ class TableViewController: UITableViewController {
         else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
+    }
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (UIAlertAction) in
+            
+            self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray,forKey: "TodoListArray")
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "New Item"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
