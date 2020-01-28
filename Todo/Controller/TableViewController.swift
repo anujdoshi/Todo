3a23425c86727c2fabd5e5ae4309160dd7dc0e73
Anuj Doshi <anuj.doshi.sa@gmail.com>
2020-01-28 10:48:06 +0530

    MVC is implemented but not add item in the user defaults

--- Todo/Controller/TableViewController.swift
+++ Todo/Controller/TableViewController.swift
@@ -9,41 +9,50 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    var itemArray = ["Buy Apple","Buy Milk"]
+    
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]
+        let newItem = Item()
+        newItem.title = "Find Milk"
+        itemArray.append(newItem)
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item]
         {
                 itemArray = items
         }
     }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Todo",for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
 
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        tableView.reloadData()
     }
+    
     @IBAction func addItem(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (UIAlertAction) in
-            
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray,forKey: "TodoListArray")
             self.tableView.reloadData()
         }
@@ -65,6 +74,7 @@ class TableViewController: UITableViewController {
 //    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
 //        return true
 //    }
+    
     override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let info = UIContextualAction(style: .normal, title: "Info") { (ac:UIContextualAction, view:UIView, sucess:(Bool) -> Void) in
             sucess(true)
@@ -72,6 +82,7 @@ class TableViewController: UITableViewController {
         info.backgroundColor = .blue
         return UISwipeActionsConfiguration(actions: [info])
     }
+    
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let delete = UIContextualAction(style: .normal, title: "Delete") { (ac:UIContextualAction, view:UIView, sucess:(Bool) -> Void) in
             sucess(true)
@@ -80,4 +91,5 @@ class TableViewController: UITableViewController {
         delete.backgroundColor = .red
         return UISwipeActionsConfiguration(actions: [delete])
     }
+    
 }
---
+++ Todo/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Anuj Doshi on 27/01/20.
+//  Copyright © 2020 Anuj Doshi. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title:String = ""
+    var done:Bool = false
+}
